9a5e05f46f47ddc4c74a04df3de81aa16a25b705
Lucas <lucas.gouvea@aluno.unifenas.br>
2024-10-26 17:21:19 -0300

    correcao

--- app/Http/Controllers/PokedexController.php
+++ app/Http/Controllers/PokedexController.php
@@ -8,8 +8,19 @@ use Illuminate\Http\Request;
 
 class PokedexController extends Controller
 {
+    public function __construct()
+    {
+    }
+
     public function index(Request $request): JsonResponse
     {
         return response()->json(ListPokemons::fromRequest($request)->execute());
+    }
+
+    public function sync(): JsonResponse
+    {
+        exec('php D:\ProjetosPhp\ApiPokedexV2\artisan app:pokedex-command > NUL 2>&1 &');
+
+        return response()->json(['message' => 'Sincronização iniciada.']);
     }
 }
